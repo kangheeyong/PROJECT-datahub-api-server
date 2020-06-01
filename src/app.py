af6484bc715d5dfd4e4631d4502fb91b60e8d60f
kang hee yong <cagojeiger@naver.com>
2020-06-01 18:41:12 +0900

    add sample code

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
 from sanic import Sanic
-from sanic.response import json
-from sanic_openapi import doc, swagger_blueprint
+from sanic_openapi import swagger_blueprint
 
-from util import authorized
-
+from sample import sample
 
 app = Sanic(__name__)
 
@@ -14,23 +12,7 @@ app.config["API_CONTACT_EMAIL"] = "cagojeiger@naver.com"
 app.config["API_TERMS_OF_SERVICE"] = "https://github.com/kangheeyong/PROJECT-datahub-api-server.git"
 app.config["API_LICENSE_NAME"] = "MIT LICENSE"
 app.blueprint(swagger_blueprint)
-
-
-class Test_status:
-    status = doc.String()
-
-
-@app.route('/test')
-@doc.tag('test')
-@doc.summary('test koken')
-@doc.description('This is a test route with detail description.')
-@doc.consumes(doc.String(name='token'), location='header', required=True)
-@doc.response(200, Test_status, description='한글도 되나?')
-@doc.response(403, Test_status, description='123aaa')
-@authorized(token='12')
-async def test(request):
-    return json({'status': 'success'})
-
+app.blueprint(sample)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8070)
